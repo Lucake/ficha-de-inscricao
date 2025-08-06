fbcd117b1e3c65486b00452eafbeeec73674da60
Lucake <trindaderaposo@hotmail.com>
2025-08-06 10:33:35 -0300

    updated partiu futuro

--- modules/project_data.js
+++ modules/project_data.js
@@ -9,42 +9,42 @@
 
 export const projects = {
   restart: {
-    name: "AWS Restart",
-    logo: "./images/logos/restart.png",
-    image: "./images/photos/esv.jpg",
-    color: "#027DBA",
-    message: "Para dar continuidade no processo, aguarde o contato da equipe",
-    howDidYouKnow: ["Amazon", "Softex"],
-    disabledFieldsId: ["input-escolaridade-2"],
+    name: 'AWS Restart',
+    logo: './images/logos/restart.png',
+    image: './images/photos/esv.jpg',
+    color: '#027DBA',
+    message: 'Para dar continuidade no processo, aguarde o contato da equipe',
+    howDidYouKnow: ['Amazon', 'Softex'],
+    disabledFieldsId: ['input-escolaridade-2'],
     cities: [
       {
-        state: "Pernambuco",
-        city: "Recife",
+        state: 'Pernambuco',
+        city: 'Recife',
       },
     ],
   },
 
   cdd: {
-    name: "Com.Dominio Digital 4.0",
-    logo: "./images/logos/cdd.png",
-    image: "./images/photos/cdd.jpg",
-    color: "#027DBA",
-    message: "Para dar continuidade no processo, aguarde o contato da equipe",
+    name: 'Com.Dominio Digital 4.0',
+    logo: './images/logos/cdd.png',
+    image: './images/photos/cdd.jpg',
+    color: '#027DBA',
+    message: 'Para dar continuidade no processo, aguarde o contato da equipe',
     howDidYouKnow: [],
     disabledFieldsId: [],
     cities: [
       {
-        state: "Pernambuco",
-        city: "Recife",
+        state: 'Pernambuco',
+        city: 'Recife',
       },
     ],
   },
 
   esv: {
-    name: "Escola Social do Varejo",
-    logo: "./images/logos/esv.png",
-    image: "./images/photos/esv.jpg",
-    color: "#17428B",
+    name: 'Escola Social do Varejo',
+    logo: './images/logos/esv.png',
+    image: './images/photos/esv.jpg',
+    color: '#17428B',
     message: `
       Entre no grupo de WhatsApp para dar continuidade à seleção.<br><br>
       <a href='https://chat.whatsapp.com/JGsQf8WQr9D6lcjq7Wsd1r'>Whatsapp - Bahia (clique aqui)</a><br>
@@ -52,29 +52,29 @@ export const projects = {
       <a href='https://chat.whatsapp.com/FGD5wZNUWjB12FFWfr3Cqv'>Whatsapp - Pernambuco (clique aqui)</a><br><br>
       <b>*é obrigatório acessar o grupo do WhatsApp para participar das etapas processo seletivo e obter mais informações<b>
     `,
-    howDidYouKnow: ["Carrefour"],
+    howDidYouKnow: ['Carrefour'],
     disabledFieldsId: [],
     cities: [
       {
-        state: "Bahia",
-        city: "Salvador",
+        state: 'Bahia',
+        city: 'Salvador',
       },
       {
-        state: "Ceará",
-        city: "Fortaleza",
+        state: 'Ceará',
+        city: 'Fortaleza',
       },
       {
-        state: "Pernambuco",
-        city: "Recife",
+        state: 'Pernambuco',
+        city: 'Recife',
       },
     ],
   },
 
   rotas: {
-    name: "Rotas e Travessias",
-    logo: "./images/logos/rotas.png",
-    image: "./images/photos/rotas.jpg",
-    color: "#f1872d",
+    name: 'Rotas e Travessias',
+    logo: './images/logos/rotas.png',
+    image: './images/photos/rotas.jpg',
+    color: '#f1872d',
     message: `
       Entre no grupo de WhatsApp para dar continuidade à seleção.<br><br>
       <a href='https://chat.whatsapp.com/JrVMV3GlbslF9KnPGIm0YU'>Whatsapp - Bahia (clique aqui)</a><br>
@@ -82,45 +82,43 @@ export const projects = {
       <a href='https://chat.whatsapp.com/JrVMV3GlbslF9KnPGIm0YU'>Whatsapp - São Paulo (clique aqui)</a><br><br>
       <b>*é obrigatório acessar o grupo do WhatsApp para participar das etapas processo seletivo e obter mais informações<b>
     `,
-    howDidYouKnow: ["Suzano", "Zurich Seguros", "Zurich Santander"],
+    howDidYouKnow: ['Suzano', 'Zurich Seguros', 'Zurich Santander'],
     disabledFieldsId: [],
     cities: [
       {
-        state: "Bahia",
-        city: "Salvador",
+        state: 'Bahia',
+        city: 'Salvador',
       },
       {
-        state: "Bahia",
-        city: "Teixeira de Freitas",
+        state: 'Bahia',
+        city: 'Teixeira de Freitas',
       },
       {
-        state: "São Paulo",
-        city: "São Paulo",
+        state: 'São Paulo',
+        city: 'São Paulo',
       },
       {
-        state: "Ceará",
-        city: "Itaitinga",
+        state: 'Ceará',
+        city: 'Itaitinga',
       },
     ],
   },
   partiufuturo: {
-    name: "Partiu Futuro",
-    logo: "./images/logos/partiu.png",
-    image: "./images/photos/partiu.png",
-    color: "#FF8930",
-    message: "Para dar continuidade no processo, aguarde o contato da equipe",
-
-    // `
-    //   Entre no grupo de WhatsApp para dar continuidade à seleção.<br><br>
-    //   <a href='https://chat.whatsapp.com/CkUJ2iMiYwE8tQdIQ7srMZ'>Grupo do Whatsapp (clique aqui)</a><br><br>
-    //   <b>*é obrigatório acessar o grupo do WhatsApp para participar das etapas processo seletivo e obter mais informações<b>
-    // `,
-    howDidYouKnow: ["Boulevard Shopping", "Zurich Seguros", "Zurich Santander"],
+    name: 'Partiu Futuro',
+    logo: './images/logos/partiu.png',
+    image: './images/photos/partiu.png',
+    color: '#FF8930',
+    message: `
+      Entre no grupo de WhatsApp para dar continuidade à seleção.<br><br>
+      <a href='https://chat.whatsapp.com/HRre3QRsduw1PLrztaYfjr?mode=ac_t'>Grupo do Whatsapp (clique aqui)</a><br><br>
+      <b>*é obrigatório acessar o grupo do WhatsApp para participar das etapas processo seletivo e obter mais informações<b>
+    `,
+    howDidYouKnow: ['Boulevard Shopping', 'Zurich Seguros', 'Zurich Santander'],
     disabledFieldsId: [],
     cities: [
       {
-        state: "Pará",
-        city: "Belém",
+        state: 'Pará',
+        city: 'Belém',
       },
     ],
   },
